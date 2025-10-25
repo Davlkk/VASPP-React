9dd4ee7b5e2809cdfda8ed45455cf1a229488c08
Davi Matheus <davimateusabreuw@gmail.com>
2025-10-25 16:03:17 -0300

    criação da pagina de cadastro

---
+++ src/pages/cadastro.tsx
@@ -0,0 +1,56 @@
+import Background from "../components/Background";
+import BlueButton from "../components/BlueButton";
+import InputTransparent from "../components/InputTransparent";
+import vaspp from "../pictures/vaspp-logo.svg";
+import facebook from "../pictures/facebook.png";
+import google from "../pictures/google.png";
+
+function Cadastro() {
+  return (
+    <Background>
+      <div className="py-[15vh] min-h-screen min-w-screen flex flex-col items-center">
+        <div>
+          <img src={vaspp} alt="vaspp-logo" className="h-auto w-[20vh]" />
+        </div>
+        <div className="min-w-screen mt-32 w-[45vh]">
+          <h1 className="text-white text-5xl font-semibold text-center">
+            Cadastre-se
+          </h1>
+        </div>
+
+        <div className="mt-12 w-auto h-auto flex flex-col gap-4 items-center">
+          <InputTransparent placeholder="Apelido" className="w-[45vh]" />
+          <InputTransparent placeholder="nome@gmail.com" className="w-[45vh]" />
+          <InputTransparent placeholder="Senha" className="w-[45vh]" />
+          <BlueButton type="submit" className="w-[45vh]" children="CRIAR CONTA" />
+
+          <div className="w-[45vh] h-auto flex justify-center items-center gap-2 text-white">
+            <hr className="w-full" />
+            <p className="w-full text-md text-center">OU ENTRE COM</p>
+            <hr className="w-full" />
+          </div>
+
+          <div className="w-[45vh] h-auto gap-4 flex items-center">
+            <div className="w-full h-auto p-2 rounded-md bg-white flex justify-center items-center cursor-pointer hover:bg-slate-300 transition-colors duration-200">
+              <img src={google} alt="google" className="w-[3vh]" />
+            </div>
+            <div className="w-full h-auto p-2 rounded-md bg-white flex justify-center items-center cursor-pointer hover:bg-slate-300 transition-colors duration-200">
+              <img src={facebook} alt="facebook" className="w-[3vh]" />
+            </div>
+          </div>
+
+          <div>
+            <p>
+              Já possui uma conta?{" "}
+              <a href="/login" className="text-[#4665FF]">
+                Entrar
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </Background>
+  );
+}
+
+export default Cadastro;
